23f01d77e226636c9086e459e360c3021643c55a
Mukul <mukulz2020@gmail.com>
2023-11-16 22:10:02 +0530

    aws domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['s3.us-west-2.amazonaws.com', 'pbs.twimg.com', 'firebasestorage.googleapis.com'],
+        domains: [
+            's3.us-west-2.amazonaws.com',
+            'pbs.twimg.com',
+            'firebasestorage.googleapis.com',
+            'prod-files-secure.s3.us-west-2.amazonaws.com'
+        ],
         dangerouslyAllowSVG: true,
         contentDispositionType: 'attachment',
         contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
