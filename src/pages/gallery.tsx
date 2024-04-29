17424d3fd0b336c8689d04937902f13e365c346a
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2024-04-29 23:11:03 +0530

    gdrive api works with major flaws

---
+++ src/pages/gallery.tsx
@@ -0,0 +1,59 @@
+
+import { GetStaticProps } from 'next';
+
+import React from 'react';
+
+interface GalleryProps {
+  images: string[]; 
+}
+
+const Photogallery: React.FC<GalleryProps> = ({ images }) => {
+  return (
+    <div>
+      <div className="photogallery-container">
+        {images.map((src, index) => (
+          <div className="photogallery-item" key={index}>
+            <img src={src} alt={`Pic ${index + 1}`} loading="lazy" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const apiKey = process.env.GOOGLE_DRIVE_API_KEY;
+    const driveLink = process.env.GOOGLE_DRIVE_API_LINK;
+
+    if (!apiKey || !driveLink) {
+      throw new Error('Google Drive API key or link not provided in environment variables');
+    }
+
+    const response = await fetch(
+      `${driveLink}&key=${apiKey}`
+    );
+    const data = await response.json();
+
+    const images = data.items.map((item: any) => item.thumbnailLink);
+
+
+      
+    return {
+      props: {
+        images,
+      },
+      revalidate: 3600, // Re-generate page every hour (you can adjust this value as needed)
+    };
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    return {
+      props: {
+        images: [],
+      },
+    };
+  }
+};
+
+export default Photogallery;
