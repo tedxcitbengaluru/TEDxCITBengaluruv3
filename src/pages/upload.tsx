610b87f0b0cd10d69f015965a9dd226f66cc6fd4
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2024-05-02 23:01:57 +0530

    css and few more functionalities in upload route

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -3,8 +3,12 @@ import { UploadButton } from '@/components/uploadButton';
 
 const UploadPhoto = () => {
     return (
-        <div className="w-[300px]">
-            <UploadButton multiple={true} />
+        <div className="upload-photo-container">
+            <div className="flex justify-center items-center h-screen">
+                <div className="w-[300px]">
+                    <UploadButton className="upload-button" multiple={true} />
+                </div>
+            </div>
         </div>
     );
 };
